4ef27749cea75bff568c80ce2066e71e117d9974
Anees <aneesaboo123@gmail.com>
2025-08-13 13:08:12 +0530

    added:add book logic

---
+++ handleBook.js
@@ -0,0 +1,29 @@
+document
+  .getElementById("addBookForm")
+  .addEventListener("submit", async function (e) {
+    e.preventDefault();
+    const bookId = document.getElementById("bookId").value;
+    const bookTitle = document.getElementById("title").value;
+    const author = document.getElementById("author").value;
+    const genre = document.getElementById("genre").value;
+    const status = document.getElementById("status").value;
+
+    console.log(bookId, bookTitle, author, genre, status);
+    try {
+      let addBook = await fetch("http://localhost:5000/addbooks", {
+        method: "POST",
+        headers: {
+          "content-type": "application/json",
+        },
+        body: JSON.stringify({ bookId, bookTitle, author, genre, status }),
+      });
+      if (addBook) {
+        window.alert("book has been successfully added");
+        window.location.href = "/adminHome";
+      } else {
+        window.alert("failed to add the book");
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  });
